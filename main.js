c061f963993660cb31377493f4bceed5cc814732
MS1920 <60628675+MS1920@users.noreply.github.com>
2020-02-09 23:46:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+function displayPyramid(n) {
+  for (var i = 0; i < 25; i++) {
+    var str = '';
+    for (var j = 1; j < 25-i; j++) {
+      str = str + ' ';
+    }
+    for (var k = 1; k <= (2*i+1); k++) {
+      str = str + '*';
+    }
+    console.log(str);
+  }
+}
